b30b0fe172c0a9a6fd9395e27da49297c62c2caa
Kaveesha Ranmuthu <kaveesha@movio.co>
2024-08-09 15:37:46 +1200

    add more styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from "typescript";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,6 +12,15 @@ export default {
         "light-gray": "#EAEAEA",
         "dark-gray": "#333333",
         orange: "#e15b1d",
+      },
+      keyframes: {
+        fade: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fade: "fade 1.5s ease-in-out",
       },
     },
   },
